ceadc5d51de4c19b82f22df25c0558b59381f43c
Stefan Schmidt <stefan.joachim.schmidt@gmail.com>
2026-02-17 06:46:22 +0100

    course 2, module 4 started

--- c2_m3_text.sql
+++ c2_m3_text.sql
@@ -312,7 +312,13 @@ EXPLAIN ANALYZE SELECT * from cr4 WHERE url = 'lemons';
 
 
 
-----------
--- DEMO --
-----------
+------------------
+-- Assignment 3 --
+------------------
+
+CREATE TABLE bigtext (
+  content TEXT
+);
+
+INSERT INTO bigtext (content) SELECT 'This is record number ' || generate_series(100000,199000) || ' of quite a few text records.';
 
